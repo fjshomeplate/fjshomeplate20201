5063a8f4521681e5a80a38800f7fb74a4a775996
fjshomeplate <38842004+fjshomeplate@users.noreply.github.com>
2020-08-28 15:40:42 -0400

    Color, SEO, etc
    Full details in code

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `F44336`,
       marginBottom: `1.45rem`,
     }}
   >
