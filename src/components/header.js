57c2cdfef035cf71b4e0d6af812b45cfb9133ddd
fjshomeplate <38842004+fjshomeplate@users.noreply.github.com>
2020-08-29 15:19:30 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `F44336`,
+      background: '#F34336',
       marginBottom: `1.45rem`,
     }}
   >
